a107823a21b3d4f31f597e27208c42bced939895
Aletsis8 <31967017+Aletsis8@users.noreply.github.com>
2017-09-15 11:04:46 -0500

    Primera ves que lo subo a Git

---
+++ Juego 1/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+
+	public float speed;             
+
+	private Rigidbody2D rb2d;       
+	public Text contText;
+	private int contador;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		contador = 0;
+		contText.text = "Candy: " + contador.ToString ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rb2d.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D (Collider2D colectible){
+
+		if (colectible.gameObject.CompareTag ("Player")) {
+
+			colectible.gameObject.SetActive (false);
+			contador += 1;
+			contText.text = "Candy: " + contador.ToString ();
+
+
+		
+		}
+	}
+
+}
---
+++ Juego 1/Assets/Recolectable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recolectable : MonoBehaviour {
+
+	public GameObject Dulce;
+	private int NumMaximo = 9;
+
+
+	void Start()
+	{
+		for (int contador = 0; contador <= NumMaximo; contador++) {
+			Vector3 position = new Vector3 (Random.Range (-5.0f, 5.0f), Random.Range (4.0f, -5.0f), 0);
+			Instantiate (Dulce, position, Quaternion.identity);
+		}
+	}
+	}
+
